a95c7ba8df866f83cacd35a524077cedb466349f
Sujal Maharjan <sujalmaharjan2059@gmail.com>
2025-03-19 11:41:51 +0545

    feat: api for youtube channels

---
+++ app/Models/SourceTier.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class SourceTier extends Model
+{
+    use HasFactory;
+
+    protected $table = 'source_tiers';
+    
+    protected $fillable = [
+        'name',
+        'slug',
+        'interval_value'
+    ];
+}
